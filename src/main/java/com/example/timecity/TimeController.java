b77e87907b29aa0f6c0976eb6e8af12519b7a342
moaherback <moa.herback@gmail.com>
2025-02-27 10:50:25 +0100

    Remove unused import

--- src/main/java/com/example/timecity/TimeController.java
+++ src/main/java/com/example/timecity/TimeController.java
@@ -6,8 +6,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.time.LocalDateTime;
-
 @Controller
 public class TimeController {
 
